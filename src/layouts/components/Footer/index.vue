c838784e717345877d0d021ffbc99b614bfdd0bc
hongda <hirotatu.ri@nassen.co.jp>
2024-09-30 12:50:01 +0900

    ce

--- src/layouts/components/Footer/index.vue
+++ src/layouts/components/Footer/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer flx-center">
-    <a href="https://github.com/HalseySpicy" target="_blank"> 2022 © nassen-Admin By nassen Technology. </a>
+    <a href="https://github.com/HalseySpicy" target="_blank"> 2024 © nassen-Admin By nassen Technology. </a>
   </div>
 </template>
 
